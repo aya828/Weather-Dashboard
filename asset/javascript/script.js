9b185bfdb3f401468aab1c3724ccfd89cdf5a623
aya <aya.yoshida8@gmail.com>
2020-01-14 09:50:02 -0500

    css updated

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-  // You don't need all these. You need a separate URL for each city, but you build it dynamically based on 
-  // the search query via the data object in line 31 below.
-  var austinURL = "http://api.openweathermap.org/data/2.5/forecast?id=4254010&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var chicagoURL = "http://api.openweathermap.org/data/2.5/forecast?id=4887398&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var nyURL = "http://api.openweathermap.org/data/2.5/forecast?id=5128638&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var orlandoURL= "http://api.openweathermap.org/data/2.5/forecast?id=4167147&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var sanfranURL= "http://api.openweathermap.org/data/2.5/forecast?id=5391959&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var seattleURL = "http://api.openweathermap.org/data/2.5/forecast?id=5809844&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var denverURL = "http://api.openweathermap.org/data/2.5/forecast?id=4853799&APPID=2633dd7957343b9edb565bc3bc9be36a";
-  var atlantaURL = "http://api.openweathermap.org/data/2.5/forecast?id=4671576&APPID=2633dd7957343b9edb565bc3bc9be36a";
 
   var apiKey = "2633dd7957343b9edb565bc3bc9be36a";
   var weatherURL = "http://api.openweathermap.org/data/2.5/forecast";
@@ -17,8 +7,6 @@ $(document).ready(function(){
   var searchBtn = $("#searchBtn");
   var input = $("#search");
   var value = input.val();
-  var lat = [];
-  var lon = [];
 
   // Variables for today's weather
   var title = $("#cityTitle");
@@ -26,7 +14,7 @@ $(document).ready(function(){
   var humidity = $("#humidity")
   var wind = $("#wind");
 
-  // Variables for 5-Day forecast
+  // Variables for 5-Day forecast temperature and humidity
   var firstTemp = $("#1Temp");
   var firstHumidity = $("#1Humidity");
   var secondTemp = $("#2Temp");
@@ -63,40 +51,41 @@ $(document).ready(function(){
         humidity.text("Humidity: " + response.list[0].main.humidity + "%");
 
         // First day of forecast
-        firstDate.text(moment().add(1, 'days').format('MMM Do'));
+        firstDate.text(moment().add(1, 'days').format('dddd, MMM Do'));
         var iconImgOne = "http://openweathermap.org/img/w/" + response.list[6].weather[0].icon + ".png";
           $("#1img").attr('src', iconImgOne);
         firstTemp.text("Temp: " + parseInt(response.list[6].main.temp_max) + String.fromCharCode(176))
         firstHumidity.text("Humidity: " + response.list[6].main.humidity + "%");
 
         // Second day of forecast
-        secondDate.text(moment().add(2, 'days').format('MMM Do'));
+        secondDate.text(moment().add(2, 'days').format('dddd, MMM Do'));
         var iconImgTwo = "http://openweathermap.org/img/w/" + response.list[13].weather[0].icon + ".png";
           $("#2img").attr('src', iconImgTwo);
         secondTemp.text("Temp: " + parseInt(response.list[13].main.temp_max) + String.fromCharCode(176))
         secondHumidity.text("Humidity: " + response.list[13].main.humidity + "%");
 
         // Third day of forecast
-        thirdDate.text(moment().add(3, 'days').format('MMM Do'));
+        thirdDate.text(moment().add(3, 'days').format('dddd, MMM Do'));
         var iconImgThree = "http://openweathermap.org/img/w/" + response.list[21].weather[0].icon + ".png";
           $("#3img").attr('src', iconImgThree);
         thirdTemp.text("Temp: " + parseInt(response.list[21].main.temp_max) + String.fromCharCode(176))
         thirdHumidity.text("Humidity: " + response.list[21].main.humidity + "%");
 
         // Fourth day of forecast
-        fourthDate.text(moment().add(4, 'days').format('MMM Do'));
+        fourthDate.text(moment().add(4, 'days').format('dddd, MMM Do'));
         var iconImgFour = "http://openweathermap.org/img/w/" + response.list[29].weather[0].icon + ".png";
           $("#4img").attr('src', iconImgFour);
         fourthTemp.text("Temp: " + parseInt(response.list[29].main.temp_max) + String.fromCharCode(176))
         fourthHumidity.text("Humidity: " + response.list[29].main.humidity + "%");
 
         // Fifth day of forecast
-        fifthDate.text(moment().add(5, 'days').format('MMM Do'));
+        fifthDate.text(moment().add(5, 'days').format('dddd, MMM Do'));
         var iconImgFive = "http://openweathermap.org/img/w/" + response.list[37].weather[0].icon + ".png";
           $("#5img").attr('src', iconImgFive);
         fifthTemp.text("Temp: " + parseInt(response.list[37].main.temp_max) + String.fromCharCode(176))
         fifthHumidity.text("Humidity: " + response.list[37].main.humidity + "%");
 
+        // UV Index ajax
         $.ajax( {
           url: UVIndexURL,
           method: "GET",
